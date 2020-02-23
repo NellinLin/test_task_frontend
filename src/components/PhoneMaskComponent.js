45e8aceffcf368269a616505c860f4cee62c60c0
Nell <ADshishova@mail.ru>
2020-02-23 14:22:01 +0300

    added validation and error processing

--- src/components/PhoneMaskComponent.js
+++ src/components/PhoneMaskComponent.js
@@ -1,9 +1,18 @@
 import './PhoneMaskComponent.css';
 
 const PHONE_CONTAINER_ERR = 'Отсутсвует тег с классом phone-container';
+const MASK_TYPE_ERR = 'Неверный тип входных данных (маска)';
 
 /**
  * Phone mask component
+ *
+ * @description
+ * Маска инпута. Значения:
+ * "I" - одиночный инпут для ввода одной цифры
+ * "X" - серый блок с символом "X"
+ * "*" - серый блок с символом "●"
+ * <цифра> - серый блок с введенной цифрой
+ * <не цифра> - символ отображается "как есть"
  */
 export default class PhoneMaskComponent {
     /**
@@ -12,6 +21,7 @@ export default class PhoneMaskComponent {
      * @param {string} mask
      */
     constructor(parent = document.body, mask = '') {
+        this.maskValidation(mask);
         this._parent = parent;
         this._mask = mask.split('');
     }
@@ -21,14 +31,22 @@ export default class PhoneMaskComponent {
      * @param {string} mask
      */
     set mask(mask = '') {
+        this.maskValidation(mask);
         this._mask = mask.split('');
     }
 
     /**
      * Render mask form
-     * @param {*} context
      */
-    render(context) {
+    render() {
+        let phoneContainer = this._parent.getElementsByClassName('phone-mask-create-container')[0];
+        if (phoneContainer) {
+            phoneContainer.innerHTML = '';
+        } else {
+            phoneContainer = document.createElement('div');
+            phoneContainer.className = 'phone-mask-create-container';
+        }
+
         const phoneField = document.createElement('div');
         phoneField.className = 'phone-container';
 
@@ -48,7 +66,14 @@ export default class PhoneMaskComponent {
                 phoneField.innerHTML += `<div class="text margin">${elem}</div>`;
             }
         });
-        this._parent.append(phoneField);
+
+        const errorField = document.createElement('div');
+        errorField.className = 'error-container error-container_invisible margin';
+        errorField.textContent = 'Неверный номер, попробуйте ещё раз';
+
+        phoneContainer.append(phoneField);
+        phoneContainer.append(errorField);
+        this._parent.append(phoneContainer);
     }
 
     /**
@@ -57,17 +82,89 @@ export default class PhoneMaskComponent {
      * @return {boolean}
      */
     checkMask(answer) {
-        const phoneField = this._parent.getElementsByClassName('phone-container');
-        if (!phoneField.length) {
-            throw new Error(PHONE_CONTAINER_ERR);
-        }
-
         const inputMask = this._mask;
-        const inputFields = phoneField[0].getElementsByTagName('input');
+
+        const inputFields = this.getHtmlCollectionInput('phone-container', 'input');
         [].forEach.call(inputFields, (elem) => {
             inputMask[elem.dataset.index] = elem.value;
         });
 
-        return inputMask.join('') === answer ? true : false;
+        if (inputMask.join('') !== answer) {
+            this.errorView(inputFields);
+            return false;
+        }
+
+        this.withoutErrorView(inputFields);
+        return true;
+    }
+
+    /**
+     * Get phone number
+     * @return {string}
+     */
+    getPhoneNumber() {
+        const inputMask = this._mask;
+
+        const inputFields = this.getHtmlCollectionInput('phone-container', 'input');
+        [].forEach.call(inputFields, (elem) => {
+            inputMask[elem.dataset.index] = elem.value;
+        });
+
+        return inputMask.join('');
+    }
+
+    /**
+     * Get HTMLCollection (tagName) from elem (className)
+     * @param {string} className
+     * @param {string} tagName
+     * @return {HTMLCollection}
+     */
+    getHtmlCollectionInput(className, tagName) {
+        const phoneField = this._parent.getElementsByClassName(className);
+        if (!phoneField.length) {
+            throw new Error(PHONE_CONTAINER_ERR);
+        }
+
+        return phoneField[0].getElementsByTagName(tagName);
+    }
+
+    /**
+     * Error view for check func
+     * @param {HTMLCollection} inputFields
+     */
+    errorView(inputFields) {
+        [].forEach.call(inputFields, (elem) => {
+            elem.className = 'phone-container__input text block-size margin phone-container__input_error';
+        });
+
+        const errorView = this._parent.getElementsByClassName('error-container error-container_invisible margin')[0];
+        if (errorView) {
+            errorView.className = 'error-container margin';
+        }
+    }
+
+    /**
+     * Without error view for check func
+     * @param {HTMLCollection} inputFields
+     */
+    withoutErrorView(inputFields) {
+        [].forEach.call(inputFields, (elem) => {
+            elem.className = 'phone-container__input text block-size margin';
+        });
+
+        const errorView = this._parent.getElementsByClassName('error-container margin')[0];
+        if (errorView) {
+            errorView.className = 'error-container error-container_invisible margin';
+        }
+    }
+
+    /**
+     * Validation of the entered mask
+     * @param {*} mask
+     */
+    maskValidation(mask) {
+        if (typeof mask !== 'string') {
+            throw new Error(MASK_TYPE_ERR);
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import PhoneMaskComponent from './components/PhoneMaskComponent.js';
 
 const app = document.getElementById('test');
-const phone = new PhoneMaskComponent(app, '+7(985)0');
+const phone = new PhoneMaskComponent(app, '+7(985)0I1');
 phone.render();
-console.log(phone.checkMask('dddd'));
-console.log(phone.checkMask('+7(985)0'));
 
-phone.mask = '+7(985)0II-**-X';
+phone.mask = '+7(906I)-II-*X';
 phone.render();
+console.log(phone.checkMask('+7(906)--*X'));
+
+phone.mask = [12, 22];
