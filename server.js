42bc7b68e3d45835385de2f5b8ec8b695131a022
Nell <ADshishova@mail.ru>
2020-02-24 20:06:45 +0300

    removed sendfile

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ app.use(express.static('dist'));
 
 const port = 3030;
 
-app.all('*', function(req, res, next) {
-    res.sendFile(__dirname + '/dist/index.html');
-});
-
 app.listen(port, function() {
     console.log(`Server listening port ${port}`);
 });
